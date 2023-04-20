4f2e44d7b766cc8f09ec5e15ba3aee25ba8f5bb1
Gina <gprovenc@unca.edu>
2023-04-20 17:36:59 -0400

    changes to portfolio

---
+++ carousel.js
@@ -0,0 +1,41 @@
+const photos = [
+    "Magazine Article Page For FYS/1.png",
+    "Magazine Article Page For FYS/2.png",
+    "Magazine Article Page For FYS/3.png",
+    "Magazine Article Page For FYS/4.png",
+    "Magazine Article Page For FYS/5.png",
+    "Magazine Article Page For FYS/6.png",
+    "Magazine Article Page For FYS/7.png",
+];
+let idx = 0;
+
+function showImage() {
+    console.log('Show image');
+    console.log(idx);
+    document.querySelector('.current-photo').innerHTML= `<img src='${photos[idx]}'/>`;
+    document.querySelector('.caption').innerHTML= `<p> Image ${idx+1} of 10 </p> `;
+}
+
+function forward() {
+    console.log(idx);
+
+    if(idx==photos.length-1){
+        idx=0;
+    } else {
+        idx +=1;
+    }
+    showImage()
+}
+
+function back() {
+    console.log(idx);
+
+    
+    if(idx==0){
+        console.log('end of the line. setting idx back to zero')
+        idx= photos.length-1;
+    } else {
+        idx -=1;
+    }
+    showImage()
+}
